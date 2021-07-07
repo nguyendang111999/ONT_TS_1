740d35eed71deea8375287340bdd9f037c3ab5f8
nguyendang111999 <nguyendang111999@gmail.com>
2021-07-07 10:00:47 +0700

    07/07/21
    TransitionTable

--- ONT_TS/Assets/Scripts/NPC/Action/ChaseActionSO.cs
+++ ONT_TS/Assets/Scripts/NPC/Action/ChaseActionSO.cs
@@ -4,8 +4,6 @@ using UnityEngine.AI;
 [CreateAssetMenu(fileName = "Chase Action", menuName = "State Machines/Actions/Chase Action")]
 public class ChaseActionSO : StateActionSO
 {
-    [SerializeField] private GameObject target;
-    public GameObject PlayerPos => target;
     protected override StateAction CreateAction() => new ChaseAction();
 }
 public class ChaseAction : StateAction
@@ -14,6 +12,7 @@ public class ChaseAction : StateAction
     private Transform _chaseTarget;
     private CharStatsSO _stat;
     private NavMeshAgent _agent;
+
     public override void Awake(StateController stateController)
     {
         detectPlayer = stateController.GetComponent<DetectPlayer>();
@@ -21,6 +20,7 @@ public class ChaseAction : StateAction
             _stat = detectPlayer.CharStatsSO();
         _agent = stateController.GetComponent<NavMeshAgent>();
     }
+    
     public override void OnStateUpdate()
     {
         _chaseTarget = detectPlayer.GetTargetPos();
