dacbd44e4db97302377d749393289f62fb542c3c
nguyendang111999 <49648263+nguyendang111999@users.noreply.github.com>
2021-09-20 07:51:27 +0700

    Update Villager 2

---
+++ ONT_TS/Assets/Scripts/Characters/Manager/EffectManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+    [SerializeField] TrailRenderer normalSlash;
+    // [SerializeField] ParticleSystem heavyAttack;
+    // [SerializeField] ParticleSystem earthAttack;
+
+    private void Start() {
+        normalSlash.gameObject.SetActive(false);
+        // heavyAttack.Stop();
+    }
+
+    void ResetRenderSetting(){
+        RenderSettings.fogMode = FogMode.Linear;
+        RenderSettings.fogStartDistance = 0f;
+        RenderSettings.fogEndDistance = 100f;
+    }
+}
